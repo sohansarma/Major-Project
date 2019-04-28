86e6ad77a0a4f26704f820251b6f9c5ddcc0295e
Lokesh Raj Arora <lokesh.slg06@gmail.com>
2019-04-28 22:31:53 +0530

    from sarma

--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -38,14 +38,14 @@ const Signin = ({onRouteChange}) => {
 		
 		return(
 		<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-           <main className="pa4 black-80">
+           <main className="auth__style pa4 black-80">
 		     <div className="measure">
 		    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 		      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
 		      <div className="mt3">
 		        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 		        <input 
-		        className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+		        className="pa2 input-reset ba bg-transparent hover-bg-black hover-blue w-100" 
 		        type="email" 
 		        name="email-address"  
 		        id="email-address" 
@@ -55,7 +55,7 @@ const Signin = ({onRouteChange}) => {
 		      <div className="mv3">
 		        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
 		        <input 
-		        className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+		        className="b pa2 input-reset ba bg-transparent hover-bg-black hover-blue w-100" 
 		        type="password" 
 		        name="password"  
 		        id="password" 
